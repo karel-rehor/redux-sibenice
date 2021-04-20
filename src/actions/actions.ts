1bb69bcecee1aed1552175f6261e9ba64b96eeae
karel-rehor <karel.k.rehor@email.cz>
2021-04-20 10:41:32 +0200

    feat: add action abstraction layer

---
+++ src/actions/actions.ts
@@ -0,0 +1,19 @@
+
+export const initialize = ({targetString, maxGuesses, maxLetters} :
+                               {targetString: string,
+                                maxGuesses: number,
+                                maxLetters: number}) => (
+    {type: 'init', payload: {targetString, maxGuesses, maxLetters}}
+)
+
+export const reset = () => ( {type: 'reset'})
+
+export const reveal = () => ( {type: 'reveal'})
+
+export const guessLetter = (letter: string) => (
+    { type: 'guessLetter', payload: letter}
+)
+
+export const guessWord = (word: string) => (
+    { type: 'guessWord', payload: word}
+)
