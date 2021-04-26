e5ef16e2a0da5bfd96aa2683b19b1116f34554b3
karel-rehor <karel.k.rehor@email.cz>
2021-04-26 17:28:32 +0200

    test: add experiments with jest mocks

--- src/test/render.test.tsx
+++ src/test/render.test.tsx
@@ -1,4 +1,5 @@
 import React, {ReactElement, ReactComponentElement} from 'react'
+import axios from 'axios'
 import { render, fireEvent, screen } from '@testing-library/react'
 import { createStore } from 'redux'
 import "@testing-library/jest-dom/extend-expect";
@@ -13,6 +14,11 @@ import {Provider} from "react-redux";
 const targetString = 'leopard'
 const MAX_GUESS = 3
 const MAX_LETTER = 10
+
+const mockCallback = jest.fn(x => x * x)
+const bareMock = jest.fn();
+
+jest.mock('axios')
 
 /*
 for debugging
@@ -79,6 +85,47 @@ describe('sample render test', () => {
 
         expect(letter.style.textDecoration).toEqual('line-through')
 
+    })
+
+    it('tries mock callback', async ()=> {
+        const myArray: number[] = [1,2,3,4,5]
+
+        myArray.forEach(mockCallback)
+
+        console.log(`DEBUG bareMock ${JSON.stringify(bareMock)}`)
+
+        console.log(`DEBUG mockCallback.mock.calls.length ${mockCallback.mock.calls.length}`)
+        expect(mockCallback.mock.calls.length).toBe(5)
+        expect(mockCallback.mock.calls[0][0]).toBe(1)
+        expect(mockCallback.mock.calls[1][0]).toBe(2)
+        expect(mockCallback.mock.calls[2][0]).toBe(3)
+        expect(mockCallback.mock.results[0].value).toBe(1)
+        expect(mockCallback.mock.results[4].value).toBe(25)
+
+        console.log(`DEBUG mockCallback.mock.calls[1] ${JSON.stringify(mockCallback.mock.calls[1])}`)
+
+        console.log(`DEBUG mockCallback.mock.results[3] ${JSON.stringify(mockCallback.mock.results[3])}`)
+
+        bareMock.mockReturnValueOnce(36)
+            .mockReturnValueOnce('Buggles')
+            .mockReturnValue({ id: "alpha", value: "Video killed the radio star" })
+
+        console.log(`DEBUG bareMock ${bareMock()}`)
+        console.log(`DEBUG bareMock ${bareMock()}`)
+        console.log(`DEBUG bareMock ${JSON.stringify(bareMock())}`)
+
+    })
+
+    it('fetches mock users', async () => {
+        const users = [{name: 'Barbara'}]
+        const resp = {data: users}
+
+        // @ts-ignore
+        const result = axios.get.mockResolvedValue(resp)
+
+        result().then((data: any) => {
+            expect(data.name).toEqual( 'Barbara')
+        })
     })
 
 })
